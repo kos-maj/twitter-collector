7184457cf8fca046246c7fce0945ba5ede61ac86
kos-maj <kosmajac@ualberta.ca>
2022-06-13 15:09:40 -0600

    Pull tweets from user within a specified time range

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
 import tweepy
 import config
 
+'''
+TO DO: Add functionality for use case 2 (get tweets from users within a set time range)
+
+-> tweepy client 'search_all_tweets(query, end_time, start_time, ...)' endpoint is only available
+to those who have been approved for the Academic Research product track.
+
+-> For now cannot implement this since only have basic developer track
+
+'''
+
 client = tweepy.Client(bearer_token=config.BEARER_TOKEN);
 
 def printUserInfo(userData):
@@ -10,20 +20,58 @@ def printUserInfo(userData):
     for user in userData:
         print('| {:20.17} | {:20} | {:<32}'.format(user.name, user.username, user.id));
 
+def printTweetInfo(tweetData):
+    for tweet in tweetData:
+        print('-'*72);
+        print(f'id:\t{tweet.id}\ntext:\t{tweet.text}');
+        print('-'*72);
+
 def main():
     usr_choice, tw_id = None, None;
 
     while (1):
-        usr_choice  = input("\nTwitter Tool Options:\n1. Get retweeters\n2. Get likers\n> ");
+        usr_choice  = input("\nTwitter Tool Options:\n1. Get retweeters\n2. Get likers\n3. Date range\n> ");
         tw_id       = input("Tweet ID: ");
 
         if (not tw_id.isdecimal() or not usr_choice.isdecimal()):
             print("\nError: all inputs must be decimal");
-        elif (usr_choice != '1' and usr_choice != '2'):
-            print("\nError: must pick option '1' or '2'");
+        elif (usr_choice not in ['1', '2', '3']):
+            print("\nError: must pick option '1', '2', or '3'");
         else:
             break;
-            
+
+    # Pull tweets from a user within a given time frame
+    if (usr_choice == '3'): 
+        usr_id = input("Username/ID: ");
+
+        print("format: 'MM/DD/YYYY'");
+        start = input("start date: ").split(sep='/');
+        end   = input("end date: ").split(sep='/');
+        
+        # Convert to integers for datetime
+        start = [int(i) for i in start];
+        end   = [int(i) for i in end];
+
+        query = f'from:{usr_id}';
+
+        # Make request to API
+        from datetime import datetime
+        tweets = client.search_recent_tweets(
+            query = query,
+            start_time = datetime(year=start[2], month=start[0], day=start[1]),
+            end_time = datetime(year=end[2], month=end[0], day=end[1])
+        );
+
+        # Error handling
+        if (tweets.data is None):
+            print("\nError: invalid author ID or username... exiting...");
+            return -1;
+        
+        # Print retrieved tweets
+        printTweetInfo(tweets.data);
+        return 0;
+
+    # Get retweeters / likers (depending on user input)
     users = client.get_retweeters(id=tw_id) if usr_choice == '1' else client.get_liking_users(id=tw_id);
     if (users.data is None):
         print("\nError: invalid tweet ID... exiting...");
