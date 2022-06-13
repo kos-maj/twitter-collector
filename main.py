3f99ed858341d8e78ae4d65d5b17e5d08425fb2c
kos-maj <kosmajac@ualberta.ca>
2022-06-13 14:11:00 -0600

    Added mini interface and functionality to get retweeters / likers

--- main.py
+++ main.py
@@ -3,18 +3,34 @@ import config
 
 client = tweepy.Client(bearer_token=config.BEARER_TOKEN);
 
-# response = client.search_recent_tweets(query=query, max_results=25, expansions=['author_id']);
-# users = {user['id']: user for user in response.includes['users']}
-# for user in users.values():
-    # print(f'id: {user.id}\nname: {user.name}\nusername: {user.username}\n\n');
+def printUserInfo(userData):
+    print('-'*72);
+    print('| {:20} | {:20} | id'.format('name', 'username'));
+    print('-'*72);
+    for user in userData:
+        print('| {:20.17} | {:20} | {:<32}'.format(user.name, user.username, user.id));
 
+def main():
+    usr_choice, tw_id = None, None;
 
-# Get users which like a given tweet
-tweet_id = "1535283632922845184"
-users = client.get_liking_users(id=tweet_id);
+    while (1):
+        usr_choice  = input("\nTwitter Tool Options:\n1. Get retweeters\n2. Get likers\n> ");
+        tw_id       = input("Tweet ID: ");
 
-# users = client.get_retweeters(id=tweet_id);       # For retweeters
+        if (not tw_id.isdecimal() or not usr_choice.isdecimal()):
+            print("\nError: all inputs must be decimal");
+        elif (usr_choice != '1' and usr_choice != '2'):
+            print("\nError: must pick option '1' or '2'");
+        else:
+            break;
+            
+    users = client.get_retweeters(id=tw_id) if usr_choice == '1' else client.get_liking_users(id=tw_id);
+    if (users.data is None):
+        print("\nError: invalid tweet ID... exiting...");
+        return -1;
 
-for user in users.data:
-    print(user.username + ' | ' + user.name);
-print(f'total count: {users.meta["result_count"]}');
+    printUserInfo(users.data);
+    return 0;
+
+if __name__ == "__main__":
+    main();
