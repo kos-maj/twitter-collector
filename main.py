138c1dabd2a10d77beff605f4d3aa4e9268b0961
kos-maj <kosmajac@ualberta.ca>
2022-07-11 15:17:21 -0600

    many changes.. multiple user pull, recent tweets, more metadata, etc. all imported to neo4j

--- main.py
+++ main.py
@@ -17,44 +17,140 @@ to those who have been approved for the Academic Research product track.
 client = tweepy.Client(bearer_token=config.BEARER_TOKEN);
 transaction_commands = []
 
-def exec_transactions():
+def exec_transactions(session):
     # Connect to neo4j db and execute commands
-    uri = "bolt://127.0.0.1:7687"
-    db_conn = GraphDatabase.driver(uri, auth=("neo4j", "test"), encrypted=False)
-    session = db_conn.session()
+
+    # uri = "bolt://127.0.0.1:7687"
+    # db_conn = GraphDatabase.driver(uri, auth=("neo4j", "test"), encrypted=False)
+    # session = db_conn.session()
     for i in transaction_commands:
         session.run(i)
+    
+    # Clear commands
+    transaction_commands.clear()
 
 def create_tweet_relation(user, tweet_id, relation):
-    username = str(user['username']).replace("'", "")
-    name     = str(user['name']).replace("'", "")
-    cmd_1    = f"MERGE (:Follower{{\
-                    username: '{username}',\
-                    user_id: '{user['id']}',\
-                    name: '{name}'\
-                }})"            
-    cmd_2   = f"MATCH (p:Follower{{username: '{username}'}}), (t:Tweet{{id: '{tweet_id}'}})\
-                CREATE (p)-[:{relation}]->(t)"
+    username = str(user['username']).replace('"', '')
+    name     = str(user['name']).replace('"', '')
+    cmd_1    = f'''MERGE (:Follower{{\
+                    username: "{username}",\
+                    user_id: "{user['id']}",\
+                    name: "{name}"\
+                }})'''
+    cmd_2   = f'''MATCH (p:Follower{{username: "{username}"}}), (t:Tweet{{id: "{tweet_id}"}})\
+                CREATE (p)-[:{relation}]->(t)'''
     
     transaction_commands.extend([cmd_1, cmd_2])
 
 def create_follows_relation(user, main_username):
-    username = str(user['username']).replace("'", "")
-    name     = str(user['name']).replace("'", "")
-    cmd_1    = f"MERGE (:Follower{{\
-                    username: '{username}',\
-                    user_id: '{user['id']}',\
-                    name: '{name}'\
-                }})"            
-    cmd_2   = f"MATCH (p1:Follower{{username: '{username}'}}), (p2:User{{username: '{main_username}'}})\
-                CREATE (p1)-[:FOLLOWS]->(p2)"
+    username = str(user['username']).replace('"', '')
+    name     = str(user['name']).replace('"', '')
+    cmd_1    = f'''MERGE (:Follower{{\
+                    username: "{username}",\
+                    user_id: "{user['id']}",\
+                    name: "{name}"\
+                }})'''            
+    cmd_2   = f'''MATCH (p1:Follower{{username: "{username}"}}), (p2:User{{username: "{main_username}"}})\
+                CREATE (p1)-[:FOLLOWS]->(p2)'''
 
     transaction_commands.extend([cmd_1, cmd_2])
+
+def extract_users(users: list):
+    # Extracts the usernames provided at the given path. Assumes each username is seperated by a newline character.
+    # 
+    # Args:     path - path to file containing user(s)
+    #           users - list in which to store the extracted user(s)
+   
+    #           exits() application if unsuccessful
+    if(len(sys.argv) != 2):
+        print('Error: must provide a file of base username network as input.')
+        exit()
+
+    path = str(sys.argv[1])
+    if(exists(path)):
+        # Read all usernames from file
+        with open(path) as fin:
+            while(1):
+                line = fin.readline()
+                if not line:
+                    break
+                else:
+                    users.append(line.rstrip())
+    else:
+        print('Error: input file does not exist.');
+        exit();
 
 def main():
 
     # TODO: look into USER replies to TWEET relationship
 
+    uri = "bolt://127.0.0.1:7687"
+    db_conn = GraphDatabase.driver(uri, auth=("neo4j", "testing123"), encrypted=False)
+    session = db_conn.session()
+
+    ### Extract usernames from file provided
+    base_usernames = []
+    # if(extract_users(base_usernames)):
+        # return -1
+
+    base_usernames = ['TenchiNFT']
+
+    for username in base_usernames:
+        user      = client.get_user(
+                        username=username, 
+                        user_fields=['created_at', 'description', 'location', 'protected', 'public_metrics']
+                    )
+        
+        if user.data is None:
+            continue;
+
+        ### Import data into Neo4j
+        transaction_commands.append(
+            # id, name, username, follower count, location, protected
+            f'''CREATE (:User{{\
+                username: "{username}",\
+                user_id: "{user.data['id']}",\
+                name: "{user.data['name']}",\
+                follower_count: "{user.data['public_metrics']['followers_count']}",\
+                following_count: "{user.data['public_metrics']['following_count']}",\
+                tweet_count: "{user.data['public_metrics']['tweet_count']}",\
+                protected: "{user.data['protected']}",\
+                created_on: "{str(user.data['created_at'])[0:10]}"\
+                }})'''
+        )
+
+        # Import followers
+        followers = client.get_users_followers(id=user.data['id'])
+        for user in followers.data:
+            create_follows_relation(user, username)
+    
+        # Import recent tweets (if existent)
+        recent_tweets = client.search_recent_tweets(query=f'from:{username}')
+
+        if recent_tweets.data:
+            for tweet in recent_tweets.data:
+                # Add tweet and authored by relation in neo4j
+                tweet_id = tweet['id']
+                transaction_commands.append(
+                    f'''MERGE (u:User{{username: "{username}"}})\
+                      MERGE (t:Tweet{{id: "{tweet_id}", text: "{tweet['text']}"}})\
+                      CREATE (u)-[:AUTHORED]->(t)'''
+                ) 
+
+                # Add likes/retweets into neo4j 
+                likes    = client.get_liking_users(id=tweet['id'])
+                retweets = client.get_retweeters(id=tweet['id'])
+                if likes.data: 
+                    for tmp_user in likes.data:
+                        create_tweet_relation(tmp_user, tweet_id, 'LIKED')
+                if retweets.data:
+                    for tmp_user in retweets.data:
+                        create_tweet_relation(tmp_user, tweet_id, 'RETWEETED')
+    
+        exec_transactions(session)
+
+    print("[+] Program finished.")
+    return 0;
     ### Fetch data from Twitter API
     username  = 'naolmb'
     user      = client.get_user(
