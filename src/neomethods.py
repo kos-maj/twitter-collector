5d6bcd12b8e6d58711138e24ae21a453c048e62e
kos-maj <kosmajac@ualberta.ca>
2022-11-16 20:08:21 -0700

    added numerous functions in neomethods for reusability and modularity, networkConstructors cleaned up

--- src/neomethods.py
+++ src/neomethods.py
@@ -1,14 +1,13 @@
 from .neoconnection import NeoConnection
+from tweepy import Paginator
 
 def update_tweet(session, tweet_data):
     # Updates a tweet which already exists within the neo4j database
     session.run("MATCH (t:Tweet {id: $id})"
-                "SET t.likes = $like_count"
-                "t.retweets: $retweet_count"
-                "t.replies: $reply_count",
+                "SET t.likes = $like_count, t.retweets = $rt_count, t.replies = $reply_count",
                 id=tweet_data['id'], 
                 like_count=tweet_data['public_metrics']['like_count'],
-                retweet_count=tweet_data['public_metrics']['retweet_count'],
+                rt_count=tweet_data['public_metrics']['retweet_count'],
                 reply_count=tweet_data['public_metrics']['reply_count']
     )
 
@@ -44,9 +43,123 @@ def user_exists(connection: NeoConnection, user_id):
         return True
     return False
 
-# def create_tweet():
+def create_tweet(connection: NeoConnection, tweet_data):
+    # Creates a new tweet within the neo4j session provided.
+    if tweet_exists(connection, tweet_data['id']):
+        return
 
-# def create_user():
+    # Push tweet to database 
+    formatted_text = str(tweet_data['text']).replace('"', "'")
+    connection.get_session().run(
+        "CREATE (t:Tweet{\
+        id: $tweet_id,\
+        author_id: $author_id,\
+        created_at: $created_at,\
+        likes: $likes,\
+        retweets: $retweets,\
+        replies: $replies,\
+        text: $text})",
+        tweet_id = tweet_data['id'], 
+        author_id = tweet_data['author_id'], 
+        created_at = tweet_data['created_at'],
+        likes = tweet_data['public_metrics']['like_count'],
+        retweets = tweet_data['public_metrics']['retweet_count'],
+        replies = tweet_data['public_metrics']['reply_count'], 
+        text = formatted_text
+    )
+
+    # Push data regarding annotated entities
+    if "entities" in tweet_data:
+        if "mentions" in tweet_data["entities"]:
+            for user in tweet_data["entities"]["mentions"]:
+                connection.get_session().run(
+                    "MERGE (u:User {username: $username, id: $id})"
+                    "MERGE (t:Tweet {id: $tweet_id})"
+                    "MERGE (t)-[:MENTIONS]->(u)",
+                    username = user['username'], 
+                    id = user['id'], 
+                    tweet_id = tweet_data['id']
+                )
+
+        if "annotations" in tweet_data["entities"]:
+            for annotation in tweet_data["entities"]["annotations"]:
+                transaction = "MERGE (a:{} {{description: $desc}})\
+                                MERGE (t:Tweet {{id: $id}})\
+                                MERGE (t)-[:ANNOTATES {{probability: $probability}}]->(a)".format(annotation['type'])
+                connection.get_session().run(
+                    transaction,
+                    desc = annotation['normalized_text'], 
+                    id = tweet_data['id'], 
+                    probability = annotation['probability']
+                )
+
+def create_user(connection:NeoConnection, client, user_data, start_date):
+    # Creates a new user within the neo4j session provided. 
+    # Also pulls all data relating to the user including their tweets and followers.
+    if user_exists(connection, user_data['id']):
+        return
+
+    # Import user
+    connection.get_session().run(
+        "CREATE (:User{\
+        username: $username,\
+        id: $id,\
+        description: $desc,\
+        name: $name,\
+        followers: $followers,\
+        following: $following,\
+        tweet_count: $tweet_count,\
+        created_on: $created_on})",
+        username = user_data['username'], 
+        id = user_data['id'], 
+        desc = user_data['description'], 
+        name = user_data['name'],
+        followers = user_data['public_metrics']['followers_count'], 
+        following = user_data['public_metrics']['following_count'],
+        tweet_count = user_data['public_metrics']['tweet_count'], 
+        created_on = str(user_data['created_at'])[0:10]
+    )
+
+    # Import followers
+    followers = client.get_users_followers(id=user_data["id"])
+    for follower in followers.data:
+        create_follower(connection, follower, user_data['id'])
+
+    # Import tweets
+    for tweets in Paginator(
+                client.get_users_tweets,
+                id=user_data["id"],
+                start_time=start_date,
+                max_results=100,
+                tweet_fields=["author_id", "created_at", "public_metrics", "entities"],
+    ):
+        if tweets.data:
+            for tweet in tweets.data:
+                create_tweet(connection, tweet)
+                create_author(connection, user_data['id'], tweet['id'])
+
+    # Add likes/retweets in neo4j
+    # likes    = client.get_liking_users(id=tweet['id'])
+    # retweets = client.get_retweeters(id=tweet['id'])
+    # if likes.data:
+    # for tmp_user in likes.data:
+    # create_tweet_relation(tmp_user, tweet_id, 'LIKED', connection)
+    # if retweets.data:
+    # for tmp_user in retweets.data:
+    # create_tweet_relation(tmp_user, tweet_id, 'RETWEETED', connection)
+
+
+def create_author(connection: NeoConnection, user_id, tweet_id):
+    # Creates author relationship between provided user and tweet
+    if (not user_exists(connection, user_id) or not tweet_exists(connection, tweet_id)):
+        raise Exception("User/Tweet non-existent - failed to create author relation")
+
+    connection.get_session().run(
+        "MATCH (u:User{id: $user_id})"
+        "MATCH (t:Tweet{id: $tweet_id})"
+        "CREATE (u)-[:AUTHORED]->(t)",
+        user_id=user_id, tweet_id=tweet_id
+    ) 
 
 def create_tweet_relation(user, tweet_id, relation, connection: NeoConnection):
     session = connection.get_session()
@@ -65,8 +178,27 @@ def create_tweet_relation(user, tweet_id, relation, connection: NeoConnection):
                 username = username, id = tweet_id, relation = relation
     )
 
-def create_follows_relation(user, main_username, connection: NeoConnection):
-    session = connection.get_session()
+def create_follower(connection: NeoConnection, follower, user_id):
+    follower_username = str(follower['username']).replace('"', "")
+    follower_name = str(follower['name']).replace('"', "")
+
+    if user_exists(connection, follower['id']):     # Follower exists as a User within graph
+        connection.get_session().run(
+            "MATCH (f:User {id: $f_id})"
+            "MATCH (u:User {id: $user_id})"
+            "CREATE (f)-[:FOLLOWS]->(u)",
+            f_id=follower['id'], user_id=user_id 
+        ) 
+    else:
+        connection.get_session().run(
+            "MERGE (f:Follower {username: $f_username, id: $f_id, name: $f_name})"
+            "MERGE (u:User {id: $user_id})"
+            "CREATE (f)-[:FOLLOWS]->(u)",
+            f_username=follower_username, f_id=follower['id'], f_name=follower_name,
+            user_id=user_id
+        )
+
+def create_follows_relation(session, user, main_username):
     username = str(user["username"]).replace('"', "")
     name = str(user["name"]).replace('"', "")
 
--- src/networkConstructors.py
+++ src/networkConstructors.py
@@ -24,46 +24,19 @@ def buildTweetNetwork(client, tweet_ids, start_date, connection: NeoConnection):
         if tweet.data is None:
             continue
 
-        username = [client.get_user(id=tweet.data["author_id"]).data["username"]]
-        if username[0] not in all_usernames:
-            all_usernames.append(username[0])
-            buildUsernameNetwork(client, username, start_date, connection)
+        author = client.get_user(id=tweet.data['author_id'])
+        author_username = [author.data['username']]
+        if author_username[0] not in all_usernames:
+            all_usernames.append(author_username[0])
+            buildUsernameNetwork(client, author_username, start_date, connection)
 
         if tweet_exists(connection, tweet_id):       # Tweet existent in graph
-            update_tweet(session, tweet.data);  
+            update_tweet(session, tweet.data)  
         else:
-            # Import tweet into neo4j
-            tweet_id = tweet.data["id"]
-            text = str(tweet.data["text"]).replace('"', "'")
-            session.run("MERGE (u:User {username: $username})"
-                        "MERGE (t:Tweet {id: $id, author_id: $author_id, created_at: $created_at, likes: $likes, retweets: $retweets, replies: $replies, text: $text})"
-                        "CREATE (u)-[:AUTHORED]->(t)",
-                        username = username, id = tweet_id, author_id = tweet.data['author_id'], created_at = tweet.data['created_at'],
-                        likes = tweet.data['public_metrics']['like_count'], retweets = tweet.data['public_metrics']['retweet_count'], 
-                        replies = tweet.data['public_metrics']['reply_count'], text = text
-            )
-            # Push data regarding mentioned users/people/places
-            if "entities" in tweet.data:
-                if "mentions" in tweet.data["entities"]:
-                    for user in tweet.data["entities"]["mentions"]:
-                        session.run("MERGE (u:User {username: $username, id: $id}"
-                                    "MERGE (t:Tweet {id: $tweet_id})"
-                                    "MERGE (t)-[:MENTIONS]->(u)",
-                                    username = user['username'], id = user['id'], tweet_id = tweet_id, 
-                        )
-
-                if "annotations" in tweet.data["entities"]:
-                    for annotation in tweet.data["entities"]["annotations"]:
-                        transaction = "MERGE (a:{} {{description: $desc}})\
-                                       MERGE (t:Tweet {{id: $id}})\
-                                       MERGE (t)-[:ANNOTATES {{probability: $probability}}]->(a)".format(annotation['type'])
-                        session.run(transaction, 
-                                    desc = annotation['normalized_text'], id = tweet_id, probability = annotation['probability']
-                        )
+            create_tweet(connection, tweet.data)
+            create_author(connection, author.data['id'], tweet_id)
 
 def buildUsernameNetwork(client, usernames, start_date, connection: NeoConnection):
-    session = connection.get_session()
-
     for username in usernames:
         user = client.get_user(
             username=username,
@@ -78,81 +51,7 @@ def buildUsernameNetwork(client, usernames, start_date, connection: NeoConnectio
         if user.data is None:
             continue
 
-        if user_exists(connection, user.data['id']):     # User existent in graph
-            update_user(session, user.data);    
+        if user_exists(connection, user.data['id']):
+            update_user(connection.get_session(), user.data);    
         else:
-            # Import user into neo4j
-            session.run("CREATE (:User{\
-                            username: $username,\
-                            id: $id,\
-                            description: $desc,\
-                            name: $name,\
-                            followers: $followers,\
-                            following: $following,\
-                            tweet_count: $tweet_count,\
-                            created_on: $created_on})",
-                        username = username, id = user.data['id'], desc = user.data['description'], name = user.data['name'],
-                        followers = user.data['public_metrics']['followers_count'], following = user.data['public_metrics']['following_count'],
-                        tweet_count = user.data['public_metrics']['tweet_count'], created_on = str(user.data['created_at'])[0:10]
-            )
-
-            # Import user's followers
-            followers = client.get_users_followers(id=user.data["id"])
-            for follower in followers.data:
-                create_follows_relation(follower, username, connection)
-
-            # Import user's recent tweets (if existent)
-            for recent_tweets in Paginator(
-                client.get_users_tweets,
-                id=user.data["id"],
-                start_time=start_date,
-                max_results=100,
-                tweet_fields=["author_id", "created_at", "public_metrics", "entities"],
-            ):
-                if recent_tweets.data:
-                    for tweet in recent_tweets.data:
-                        # Add tweet and authored by relation in neo4j
-                        tweet_id = tweet["id"]
-                        text = str(tweet["text"]).replace('"', "'")
-                        session.run("MERGE (u:User{username: $username})"
-                                    "MERGE (t:Tweet{\
-                                        id: $tweet_id,\
-                                        author_id: $author_id,\
-                                        created_at: $created_at,\
-                                        likes: $likes,\
-                                        retweets: $retweets,\
-                                        replies: $replies,\
-                                        text: $text})"
-                                    "CREATE (u)-[:AUTHORED]->(t)",
-                                    username = username, tweet_id = tweet_id, author_id = tweet['author_id'], created_at = tweet['created_at'],
-                                    likes = tweet['public_metrics']['like_count'], retweets = tweet['public_metrics']['retweet_count'],
-                                    replies = tweet['public_metrics']['reply_count'], text = text
-                        )
-                        # Push data regarding mentioned users/people/places
-                        if "entities" in tweet:
-                            if "mentions" in tweet["entities"]:
-                                for user in tweet["entities"]["mentions"]:
-                                    session.run("MERGE (u:User {username: $username, id: $id})"
-                                                "MERGE (t:Tweet {id: $tweet_id})"
-                                                "MERGE (t)-[:MENTIONS]->(u)",
-                                                username = user['username'], id = user['id'], tweet_id = tweet_id
-                                    )
-
-                            if "annotations" in tweet["entities"]:
-                                for annotation in tweet["entities"]["annotations"]:
-                                    transaction = "MERGE (a:{} {{description: $desc}})\
-                                                   MERGE (t:Tweet {{id: $id}})\
-                                                   MERGE (t)-[:ANNOTATES {{probability: $probability}}]->(a)".format(annotation['type'])
-                                    session.run(transaction,
-                                                desc = annotation['normalized_text'], id = tweet_id, probability = annotation['probability']
-                                    )
-
-                        # Add likes/retweets in neo4j
-                        # likes    = client.get_liking_users(id=tweet['id'])
-                        # retweets = client.get_retweeters(id=tweet['id'])
-                        # if likes.data:
-                        # for tmp_user in likes.data:
-                        # create_tweet_relation(tmp_user, tweet_id, 'LIKED', connection)
-                        # if retweets.data:
-                        # for tmp_user in retweets.data:
-                        # create_tweet_relation(tmp_user, tweet_id, 'RETWEETED', connection)
+            create_user(connection, client, user.data, start_date)
